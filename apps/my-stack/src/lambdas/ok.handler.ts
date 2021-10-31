9a132df0bf884ab532b24b7ac9d751219379daed
Robert Gill <envman@gmail.com>
2021-10-31 15:42:25 +0000

    update

--- apps/my-stack/src/lambdas/ok.handler.ts
+++ apps/my-stack/src/lambdas/ok.handler.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('testing')
+  console.log('Will it update?')
 
   return {
     statusCode: 200,
