dde4ed2e7856aeefde6826cc67aa1c0fde54c891
Robert Gill <envman@gmail.com>
2021-10-31 20:17:09 +0000

    add public bucket

--- apps/my-stack/src/stacks/app-stack.ts
+++ apps/my-stack/src/stacks/app-stack.ts
@@ -3,12 +3,18 @@ import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
 import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 
+import { PublicBucket } from '@testing/cdk-constructs';
+
 export class AppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     new NodejsFunction(this, 'test-function', {
       entry: path.join(__dirname, '..', 'lambdas', 'ok.handler.ts')
+    })
+
+    new PublicBucket(this, 'public-bucket-testing-red', {
+      
     })
   }
 }
--- libs/cdk-constructs/src/lib/cdk-constructs.ts
+++ libs/cdk-constructs/src/lib/cdk-constructs.ts
@@ -1,3 +1,23 @@
-export function cdkConstructs(): string {
-  return 'cdk-constructs';
+import * as cdk from '@aws-cdk/core'
+import * as s3 from '@aws-cdk/aws-s3'
+import * as iam from '@aws-cdk/aws-iam'
+
+export type PublicBucketProps = Omit<s3.BucketProps, 'blockPublicAccess'>
+
+const publicBucketPrincipal = new iam.AnyPrincipal()
+export class PublicBucket extends s3.Bucket {
+  constructor(scope: cdk.Construct, id: string, props: PublicBucketProps) {
+    super(scope, id, {
+      ...props,
+      blockPublicAccess: new s3.BlockPublicAccess({ restrictPublicBuckets: false }),
+    })
+
+    const publicBucketPolicy = new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [`${this.bucketArn}/*`],
+      principals: [publicBucketPrincipal],
+    })
+
+    this.addToResourcePolicy(publicBucketPolicy)
+  }
 }
