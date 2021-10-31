3917a28d30d8c3919cba7235a918af848f12f7d8
Robert Gill <envman@gmail.com>
2021-10-31 11:37:56 +0000

    trigger test

--- apps/my-stack/src/lambdas/ok.handler.ts
+++ apps/my-stack/src/lambdas/ok.handler.ts
@@ -1,6 +1,8 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('testing')
+
   return {
     statusCode: 200,
     body: 'OK'
